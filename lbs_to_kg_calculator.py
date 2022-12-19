c9884307816b0cf739d3c66d290e649819be19d9
Yasim <91527193+Yasimcd@users.noreply.github.com>
2022-12-18 18:36:32 -0800

    Add files via upload

---
+++ lbs_to_kg_calculator.py
@@ -0,0 +1,13 @@
+# ***Question 3:***
+pouns = float(input("Please enter the mass in lb that you would like to convert to kg: "))
+kg = 2.20462 
+new_mass = pouns / kg
+print("The converted mass in kg is:",pouns / kg)
+earth = new_mass * 9.807
+print("Your weight on Earth is:",earth, "Newtons")
+moon = new_mass * 1.62
+print("Your weight on the Moon is:",moon, "Newtons")
+per_moon_earth = moon / earth * 100  #the percentage of the weight on the moon in comparison to what is experience on earth
+print("The percentage of the weight on the Moon in comparison to what is experience on Earth:",per_moon_earth, "%")
+print("The percentage of the weight on the Moon in comparison to what is experience on Earth as an integer is",round(per_moon_earth), "%")
+
